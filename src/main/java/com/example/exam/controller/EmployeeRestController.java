b8c3fe8456d504d8573839a221bb4eb49e77311d
DANGANHSON03 <anhson22062003xxx@gmail.com>
2025-12-11 19:36:00 +0700

    fixx

--- src/main/java/com/example/exam/controller/EmployeeRestController.java
+++ src/main/java/com/example/exam/controller/EmployeeRestController.java
@@ -15,34 +15,19 @@ public class EmployeeRestController {
     
     @Autowired
     private EmployeeService employeeService;
-    
-    /**
-     * Web service method: getEmployees - retrieve all employees in an array
-     * @return array of all employees
-     */
     @GetMapping
     public ResponseEntity<Employee[]> getEmployees() {
         List<Employee> employees = employeeService.getEmployees();
         Employee[] employeeArray = employees.toArray(new Employee[0]);
         return ResponseEntity.ok(employeeArray);
     }
-    
-    /**
-     * Web service method: addEmployees - add a new employee record
-     * @param employee the employee to add
-     * @return the saved employee
-     */
+
     @PostMapping
     public ResponseEntity<Employee> addEmployees(@RequestBody Employee employee) {
         Employee savedEmployee = employeeService.addEmployees(employee);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedEmployee);
     }
-    
-    /**
-     * Web service method: updateEmployee - modify an existing employee record
-     * @param employee the employee to update
-     * @return the updated employee
-     */
+
     @PutMapping
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
         try {
